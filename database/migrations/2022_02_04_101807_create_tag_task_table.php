3475766bf5a368335d23a64fd096013a9e3f26a3
Sofia <sofia.g.per@gmail.com>
2022-02-04 13:10:38 +0300

    Прописаны миграции для создания таблиц tags, tasks, tasks_has_tags

---
+++ database/migrations/2022_02_04_101807_create_tag_task_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_task', function (Blueprint $table) {
+            $table->foreignId('task_id');
+            $table->foreignId('tag_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_task');
+    }
+}
