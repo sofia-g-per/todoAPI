464259520c5a99d9564dad02ab8e988f2aef5b7a
Sofia <sofia.g.per@gmail.com>
2022-02-04 15:35:58 +0300

    Рефакторинг TaskController в TasksController

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,7 +6,7 @@ use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class TaskController extends Controller
+class TasksController extends Controller
 {
     public function showTasks() {
         $tasks = Task::all();
--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TagsController;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TasksController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/tasks', [TaskController::class, 'showTasks']);
+Route::get('/tasks', [TasksController::class, 'showTasks']);
 
 Route::get('/tags', [TagsController::class, 'showTags']);
 
 Route::get('/tag/{id}', [TagsController::class, 'showTag']);
 
-Route::get('/task/{id}', [TaskController::class, 'showTask']);
+Route::get('/task/{id}', [TasksController::class, 'showTask']);
 
-Route::post('/add-task', [TaskController::class, 'addTask']);
+Route::post('/add-task', [TasksController::class, 'addTask']);
 
 Route::post('/add-tag', [TagsController::class, 'addTag']);
 
